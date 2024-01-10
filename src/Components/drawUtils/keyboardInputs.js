d435d9372fb32a1ce62144bf8f2f9ae3d54f6e4f
banuka <banukaramanayaka76@gmail.com>
2024-01-10 19:43:48 +0530

    chor:add movement boundary

--- src/Components/drawUtils/keyboardInputs.js
+++ src/Components/drawUtils/keyboardInputs.js
@@ -1,24 +1,28 @@
 import { Body } from "matter-js";
 import usePlayerState from "../gameState/usePlayerState";
+import useGameStore from "../gameState/useGameStore";
 
 
 function keyboardInputs(p5) {
+    const ch = useGameStore.getState().ch
+    const cw = useGameStore.getState().cw
     const player = usePlayerState.getState().player
     const speedX = usePlayerState.getState().playerVelocityX
     const speedY = usePlayerState.getState().playerVelocityY
     // console.log(p5.keyCode);
 
     const position = player.position
-    if (p5.keyIsDown(65) || p5.keyIsDown(37)) {
+
+    if ((p5.keyIsDown(65) || p5.keyIsDown(37)) && position.x - 25 > 0) {
         Body.setPosition(player, { x: position.x - speedX, y: position.y })
     }
-    if (p5.keyIsDown(68) || p5.keyIsDown(39)) {
+    if ((p5.keyIsDown(68) || p5.keyIsDown(39)) && position.x + 25 < cw) {
         Body.setPosition(player, { x: position.x + speedX, y: position.y })
     }
-    if (p5.keyIsDown(87) || p5.keyIsDown(38)) {
+    if ((p5.keyIsDown(87) || p5.keyIsDown(38)) && position.y - 25 > 0) {
         Body.setPosition(player, { x: position.x, y: position.y - speedY })
     }
-    if (p5.keyIsDown(83) || p5.keyIsDown(40)) {
+    if ((p5.keyIsDown(83) || p5.keyIsDown(40)) && position.y + 25 < ch) {
         Body.setPosition(player, { x: position.x, y: position.y + speedY })
     }
 
