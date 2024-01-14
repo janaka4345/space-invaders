0a92bac231223e50f51fb4b73238062d09448fef
banuka <banukaramanayaka76@gmail.com>
2024-01-14 07:34:10 +0530

    other:test score and health of player

--- src/Components/drawUtils/drawPlayer.js
+++ src/Components/drawUtils/drawPlayer.js
@@ -1,5 +1,8 @@
+import usePlayerState from "../gameState/usePlayerState";
 
 export default function drawPlayer(body, p5) {
+    // const playerHealth = usePlayerState.getState().health
+    const score = usePlayerState.getState().score
     p5.push();
     p5.fill(0, 0, 255);
     // p5.rectMode(p5.CENTER);
@@ -15,5 +18,15 @@ export default function drawPlayer(body, p5) {
         body.vertices[3].y
     );
     p5.pop();
+    p5.push()
+    p5.fill(0);
+    p5.textSize(32);
+    p5.text(body.health, body.vertices[0].x, body.vertices[0].y)
+    p5.pop()
+    p5.push()
+    p5.fill(255, 0, 0);
+    p5.textSize(32);
+    p5.text(score, body.vertices[1].x, body.vertices[1].y)
+    p5.pop()
 
 }
--- src/Components/gameUtils/handleCollisions.js
+++ src/Components/gameUtils/handleCollisions.js
@@ -1,13 +1,14 @@
 import { Body } from "matter-js"
 import useGameStore from "../gameState/useGameStore"
 import usePlayerState from "../gameState/usePlayerState"
-
+//
 const engine = useGameStore.getState().engine
 const setScore = usePlayerState.getState().setScore
+// const setHealth = usePlayerState.getState().setHealth
 
 export default function handleCollisions(p5) {
     engine.detector.pairs.collisionActive.forEach((pair) => {
-        //reseting the enemy upon collision
+        //reseting the projectiles upon collision with  enemy
         if ((pair.bodyA.label === "enemiesEnter" && pair.bodyB.label === "projectilesFired")) {
             Body.setPosition(pair.bodyB, { x: - Math.random() * 120, y: - Math.random() * 120 })
             Body.setVelocity(pair.bodyB, { x: 0, y: 0 })
@@ -26,6 +27,28 @@ export default function handleCollisions(p5) {
             pair.bodyB.health -= 20
             pair.bodyA.label = 'projectiles'
         }
+        //enemy projectile and player collision
+        if ((pair.bodyA.label === "player" && pair.bodyB.label === "enemyProjectileFired")) {
+            Body.setPosition(pair.bodyB, { x: - Math.random() * 120 - 120, y: - Math.random() * 120 - 120 })
+            Body.setVelocity(pair.bodyB, { x: 0, y: 0 })
+
+            //decrese enemy health
+            pair.bodyA.health -= 5
+            // setHealth(-5)
+
+            pair.bodyB.label = 'enemyProjectiles'
+        }
+        if ((pair.bodyB.label === "player" && pair.bodyA.label === "enemyProjectileFired")) {
+            Body.setPosition(pair.bodyA, { x: - Math.random() * 120 - 120, y: - Math.random() * 120 - 120 })
+            Body.setVelocity(pair.bodyA, { x: 0, y: 0 })
+
+            //decrese enemy health
+            pair.bodyB.health -= 5
+            // setHealth(-5)
+
+            pair.bodyA.label = 'enemyProjectiles'
+        }
+
 
     })
 }
